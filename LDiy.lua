0ab2f3acbb54361e460c84c90d480d88bd64399b
hookmetamethod_hook <st3v3bl0ks@gmail.com>
2025-05-03 01:54:12 +0000

    Update LDiy.lua

--- LDiy.lua
+++ LDiy.lua
@@ -6907,8 +6907,9 @@ addcmd("serverhop", {"shop"}, function(args, speaker)
         local servers = {}
         local req = httprequest({Url = string.format("https://games.roblox.com/v1/games/%d/servers/Public?sortOrder=Desc&limit=100&excludeFullGames=true", PlaceId)})
         local body = HttpService:JSONDecode(req.Body)
-        if body then
-            for i, v in next, body do
+
+        if body and body.data then
+            for i, v in next, body.data do
                 if type(v) == "table" and tonumber(v.playing) and tonumber(v.maxPlayers) and v.playing < v.maxPlayers and v.id ~= JobId then
                     table.insert(servers, 1, v.id)
                 end
