9e16e6ee7d00cdc50b9052eb21997d940e1286fa
hookmetamethod_hook <st3v3bl0ks@gmail.com>
2025-05-08 12:18:32 +0000

    Update LDiy.lua

--- LDiy.lua
+++ LDiy.lua
@@ -25,7 +25,7 @@ if not game:IsLoaded() then
     notLoaded:Destroy()
 end
 
-currentVersion = '1.8.8'
+currentVersion = '1.8.9'
 
 local guiScale = 1 -- lazy fix for bug lol
 
@@ -15307,9 +15307,8 @@ task.spawn(function()
 	end
 	if PlaceId == 8286149869 then
 		notify("Therapy 🔊", "Game Detected!\nRun \"DisableCustomAnticheat therapy\" to disable Therapy 🔊\'s anticheat.")
-		task.wait(5)
-		notify("Therapy 🔊", "Game Detected!\nAdded \"getbadges\" and \"gotoarea\"")
-		
+		--[[task.wait(5)
+		notify("Therapy 🔊", "Game Detected!\nAdded \"getbadges\" and \"gotoarea\"")]]
 	end
 end)
 
