9520f7de7db4ef9f6b28703d3ac3e0d63198b1cf
hookmetamethod_hook <st3v3bl0ks@gmail.com>
2025-10-21 10:37:42 +0000

    made spoofspeed arg1 optional
    now u can actually use the dynamic spoof stuff to bypass detection
    
    im so retarded idk how i forgot that lmfao

--- LDiy.lua
+++ LDiy.lua
@@ -25,7 +25,7 @@ if not game:IsLoaded() then
     notLoaded:Destroy()
 end
 
-currentVersion = '1.9.13'
+currentVersion = '1.9.15'
 
 local guiScale = 1 -- lazy fix for bug lol
 
@@ -11593,28 +11593,30 @@ addcmd('speed',{'ws','walkspeed'},function(args, speaker)
 end)
 
 addcmd('spoofspeed',{'spoofws','spoofwalkspeed'},function(args, speaker)
-    if args[1] and isNumber(args[1]) then
-        if hookmetamethod then
-            local char = speaker.Character
-            local setspeed = char.Humanoid.WalkSpeed;
-			local newindex; newindex = hookmetamethod(game, "__newindex", function(self, key, value)
-                local keyclean = string.gsub(key, "\0", "")
-                if (keyclean == "WalkSpeed" or keyclean == "walkSpeed") and self:IsA("Humanoid") and self:IsDescendantOf(char) and not checkcaller() then
-                    setspeed = tonumber(value)
-                    return setspeed
-                end
-                return newindex(self, key, value)
-            end)
-            local index; index = hookmetamethod(game, "__index", function(self, key)
-                local keyclean = key:gsub("\0", "")
-                if (keyclean == "WalkSpeed" or keyclean == "walkSpeed") and self:IsA("Humanoid") and self:IsDescendantOf(char) and not checkcaller() then
-                    return setspeed or args[1]
-                end
-                return index(self, key)
-            end)
-        else
-            notify('Incompatible Exploit','Your exploit does not support this command (missing hookmetamethod)')
-        end
+    if hookmetamethod then
+		if not isNumber(args[1]) then return end
+        local char = speaker.Character
+        local setspeed = char.Humanoid.WalkSpeed;
+		local newindex; newindex = hookmetamethod(game, "__newindex", function(self, key, value)
+            local keyclean = string.gsub(key, "\0", "")
+            if (keyclean == "WalkSpeed" or keyclean == "walkSpeed") and self:IsA("Humanoid") and self:IsDescendantOf(char) and not checkcaller() then
+                setspeed = tonumber(value)
+                return setspeed
+            end
+            return newindex(self, key, value)
+        end)
+        local index; index = hookmetamethod(game, "__index", function(self, key)
+            local keyclean = key:gsub("\0", "")
+            if (keyclean == "WalkSpeed" or keyclean == "walkSpeed") and self:IsA("Humanoid") and self:IsDescendantOf(char) and not checkcaller() then
+                return setspeed or args[1]
+            end
+            return index(self, key)
+        end)
+		if args[1] then
+			notify('spoof speed', 'Hooked index & newindex')
+		end
+    else
+        notify('Incompatible Exploit','Your exploit does not support this command (missing hookmetamethod)')
     end
 end)
 
